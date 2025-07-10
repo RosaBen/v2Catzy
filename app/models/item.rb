2b7d874bf48c9d670e5699921a3ae2c70715949c
RosaBen <benchabane.rosa@outlook.com>
2025-07-10 17:02:59 +0200

    added devise

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :order_items, dependent: :destroy
   has_many :orders, through: :order_items
-  has_many :cart_items 
+  has_many :cart_items
   has_many :carts, through: :cart_items
 
   validates :description, length: { maximum: 500 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,15 +88,14 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  #config Mailjet
+  # config Mailjet
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'in-v3.mailjet.com',
+    address: "in-v3.mailjet.com",
     port: 587,
-    user_name: ENV['MAILJET_API_KEY'],      
-    password: ENV['MAILJET_SECRET_KEY'],    
+    user_name: ENV["MAILJET_API_KEY"],
+    password: ENV["MAILJET_SECRET_KEY"],
     authentication: :plain,
     enable_starttls_auto: true
   }
-
 end
