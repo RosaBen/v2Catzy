ad2bd6d36d627bfd863cbe0c92cee3aae6f5d65a
RosaBen <benchabane.rosa@outlook.com>
2025-07-10 17:27:54 +0200

    added controllers and models

---
+++ app/controllers/cart_items_controller.rb
@@ -0,0 +1,28 @@
+class CartItemsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    cart = current_user.cart || current_user.create_cart
+    item = Item.find(params[:item_id])
+
+    if cart.items.include?(item)
+      redirect_to item_path(item), notice: "Cet article est déjà dans votre panier."
+    else
+      cart.items << item
+      redirect_to item_path(item), notice: "Article ajouté au panier."
+    end
+  end
+
+  def destroy
+  cart = current_user.cart
+  item = Item.find(params[:item_id])
+
+  if cart && item
+    cart.items.destroy(item)
+    redirect_to cart_path, notice: "Article supprimé du panier."
+  else
+    redirect_to cart_path, alert: "Impossible de supprimer l'article."
+  end
+end
+
+end
